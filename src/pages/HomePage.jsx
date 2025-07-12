2b4b774cbc470990a6a9d0cab0da9869adb2d921
Htech00 <htechsolutionz@gmail.com>
2025-07-12 12:14:50 +0100

    j

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -58,7 +58,7 @@ const HomePage = () => {
                 <span className="text-[#ffffff]/70">{"};"}</span>
               </code>
             </pre>
-            <a href="/public/Henry  Cv.pdf" className="flex items-center  gap-2 mx-auto  px-4 py-2 md:w-[166.8px] bg-[#3691c5] rounded-md text-white font-bold w-[200px] justify-center ">
+            <a href="/Henry  Cv.pdf" className="flex items-center  gap-2 mx-auto  px-4 py-2 md:w-[166.8px] bg-[#3691c5] rounded-md text-white font-bold w-[200px] justify-center ">
               <div className="flex items-center gap-2">
                 <p>Download CV</p>
                 <CgSoftwareDownload className="text-[30px]" />
